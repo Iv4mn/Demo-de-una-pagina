5f7dac21b9f30ed4c6c2b6c03a32e877c24dec22
Galletitaw <73406168+LaGalletita@users.noreply.github.com>
2021-05-11 16:39:47 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function modo()
+
+{
+
+
+
+var modooscuro = document.getElementsByClassName('body');
+
+for (var i = 0; i < modooscuro.length; i++){
+	modooscuro[i].classList.toggle('black');
+
+		}
+
+}
