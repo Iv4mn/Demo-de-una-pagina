8d988f664331d7955f8359999ece049af39c31c5
Rony Stico <ronystico@gmail.com>
2021-05-12 19:54:27 -0400

    el modo oscuro persiste, arreglado enlace del correo

--- js/script.js
+++ js/script.js
@@ -1,44 +1,36 @@
+document.getElementById('boton').addEventListener('click',modo);
+document.addEventListener('DOMContentLoaded', cambiar)
 function modo()
-
 {
-
-
-
-var modooscuro = document.getElementsByClassName('body');
-
-for (var i = 0; i < modooscuro.length; i++){
-	modooscuro[i].classList.toggle('black');
-
-		}
-
+    if (localStorage.getItem('dark') === null){
+        localStorage.setItem('dark', true);
+        cambiar();
+    }
+    else {
+        localStorage.removeItem('dark');
+        cambiar();
+    }
 }
+function cambiar(){
+    if ('dark' in localStorage){
+        var modooscuro = document.getElementsByClassName('body');
 
-function fix()
+        for (var i = 0; i < modooscuro.length; i++){
+            modooscuro[i].classList.toggle('black');
+        }
 
-{
+        var modooscurocontacto = document.getElementsByClassName('contacto');
 
+        for (var i = 0; i < modooscurocontacto.length; i++){
+            modooscurocontacto[i].classList.toggle('fixed');
+            modooscuro[i].classList.toggle('bordes');
+        }
+    }
+    else {
+        var modooscuro = document.getElementsByClassName('body');
 
-
-var modooscuro = document.getElementsByClassName('contacto');
-
-for (var i = 0; i < modooscuro.length; i++){
-	modooscuro[i].classList.toggle('fixed');
-
-		}
-
-}
-
-function fix2()
-
-{
-
-
-
-var modooscuro = document.getElementsByClassName('contacto');
-
-for (var i = 0; i < modooscuro.length; i++){
-	modooscuro[i].classList.toggle('bordes');
-
-		}
-
+        for (var i = 0; i < modooscuro.length; i++){
+            modooscuro[i].classList.remove('black');
+        }
+    }
 }
